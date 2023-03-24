2d92ef83011017200c940a67448c54fc9727dd67
barry-kn <carrybarryklein49@gmail.com>
2023-03-24 09:36:37 +0300

    1st commiy

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const input = document.getElementById('button');
 function clickAlert() {
   alert('I was clicked!');
 }
-
+//////////////////////////////////////////
 input.addEventListener('click', clickAlert);
+
+function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    console.log('Button clicked!');
+  });
+}
